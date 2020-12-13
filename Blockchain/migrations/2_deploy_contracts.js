f8b041a32be0ceaa8fc4e6439f0991201db48494
NoamRodrik <rodriknoam@gmail.com>
2020-12-13 19:23:34 +0200

    Finally deployed something

--- Blockchain/migrations/2_deploy_contracts.js
+++ Blockchain/migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+var DNSRegistry = artifacts.require("DNSRegistry");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(DNSRegistry);
 };
