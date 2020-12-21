8a37d17d7289a0b616c54c4e617548381f48f0ff
pwh589 <idanba5@walla.co.il>
2020-12-22 00:25:12 +0200

    After testing added send ability, as well better docummentation

---
+++ DNS/DNSUDPServer.py
@@ -0,0 +1,61 @@
+"""DNSUDPServer.py
+
+This script sets up a DNS server - it recieves requests in port 53,
+decodes them, asks the blockchain for answers, and sends replies.
+"""
+
+import socket
+import dnslib
+
+# DNS server interface IP.
+IP = "127.0.0.1"
+
+# DNS server port.
+PORT = 53
+
+# Packet buffer size. buffer size 512 as mentioned in DNS RFC.
+BUFFER_SIZE = 512
+
+def lookup_name(name):
+    """
+    Recives a name, looks it up in the blockchain,
+    returns corresponding address if found.
+    """
+    return("1.2.3.4")
+
+def main():
+
+    # Creates internet UDP socket.
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    # Binds to the specified IP and port.
+    sock.bind((IP, PORT))
+
+    while True:
+
+        # Recives data (DNS request) and address from a client. buffer size 512 as mentioned in DNS RFC.
+        data, addr = sock.recvfrom(BUFFER_SIZE)
+
+        # Informs in the console about a request recived.
+        print ('Recieved request from {0} at port {1}.'.format(addr[0], addr[1]))
+
+        # Decodes data to DNS request.
+        dns_req = dnslib.DNSRecord.parse(data)
+
+        # For each question in the clients DNS request.
+        for question in dns_req.questions:
+
+            # Gets the name queried in the question.
+            q_name = question.get_qname()
+
+            # Generates a reply to the client.
+            dns_rep = dnslib.DNSRecord(dnslib.DNSHeader(id=dns_req.header.id,qr=1,aa=1,ra=1),q=dnslib.DNSQuestion(q_name),a=dnslib.RR(q_name,rdata=dnslib.A(lookup_name(q_name))))
+
+            # Sends the reply generated to the client.
+            sock.sendto(bytes(dns_rep.pack()), addr)
+
+            # Informs in the console about a reply sent.
+            print ('Sent reply to {0} at port {1}.\n'.format(addr[0], addr[1]))
+
+if __name__ == "__main__":
+    main()
