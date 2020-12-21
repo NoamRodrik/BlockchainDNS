d58dcdbf33a77a22730139cb9e0fff3d044f3537
pwh589 <idanba5@walla.co.il>
2020-12-22 00:54:33 +0200

    Added some exception handleing and TODO

--- DNS/DNSUDPServer.py
+++ DNS/DNSUDPServer.py
@@ -2,6 +2,9 @@
 
 This script sets up a DNS server - it recieves requests in port 53,
 decodes them, asks the blockchain for answers, and sends replies.
+
+TODO: Add API to blockchain, test if handles all types of requests and questions f.e A, AAAA reversed, other dns logics etc.
+TODO: Handle exceptions such as no connection to blockchain, address not found(send no-existent domain)
 """
 
 import socket
@@ -21,6 +24,7 @@ def lookup_name(name):
     Recives a name, looks it up in the blockchain,
     returns corresponding address if found.
     """
+    
     return("1.2.3.4")
 
 def main():
@@ -39,23 +43,41 @@ def main():
         # Informs in the console about a request recived.
         print ('Recieved request from {0} at port {1}.'.format(addr[0], addr[1]))
 
-        # Decodes data to DNS request.
-        dns_req = dnslib.DNSRecord.parse(data)
+        try:
+            
+            # Decodes data to DNS request.
+            dns_req = dnslib.DNSRecord.parse(data)
+        
+        except:
+        
+            # Informs in the console that the request is corrupted.
+            print("Failed decoding the request due to corruption.")
+        
+        else:
+            
+            # For each question in the clients DNS request:
+            for question in dns_req.questions:
 
-        # For each question in the clients DNS request.
-        for question in dns_req.questions:
+                # Gets the name queried in the question.
+                q_name = question.get_qname()
 
-            # Gets the name queried in the question.
-            q_name = question.get_qname()
-
-            # Generates a reply to the client.
-            dns_rep = dnslib.DNSRecord(dnslib.DNSHeader(id=dns_req.header.id,qr=1,aa=1,ra=1),q=dnslib.DNSQuestion(q_name),a=dnslib.RR(q_name,rdata=dnslib.A(lookup_name(q_name))))
-
-            # Sends the reply generated to the client.
-            sock.sendto(bytes(dns_rep.pack()), addr)
-
-            # Informs in the console about a reply sent.
-            print ('Sent reply to {0} at port {1}.\n'.format(addr[0], addr[1]))
+                # Generates a reply to the client.
+                dns_rep = dnslib.DNSRecord(dnslib.DNSHeader(id=dns_req.header.id,qr=1,aa=1,ra=1),q=dnslib.DNSQuestion(q_name),a=dnslib.RR(q_name,rdata=dnslib.A(lookup_name(q_name))))
+                
+                try:
+                
+                    # Sends the reply generated to the client.
+                    sock.sendto(bytes(dns_rep.pack()), addr)
+                
+                except:
+                
+                    # Informs in the console that sending the reply failed.
+                    print("Failed sending reply.")
+                
+                else:
+                
+                    # Informs in the console about a reply sent.
+                    print ('Sent reply to {0} at port {1}.\n'.format(addr[0], addr[1]))
 
 if __name__ == "__main__":
     main()
